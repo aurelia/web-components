67aa7cc189679b99763259cc010e93666eef8846
Rob Eisenberg <rob@bluespire.com>
2016-01-18 12:57:52 -0500

    feat(all): proxy bindable and methods from element to view model

--- src/component-registry.js
+++ src/component-registry.js
@@ -74,8 +74,48 @@ export class ComponentRegistry {
       }
     });
 
-    //proxy methods (without _) and bindable properties to view model
+    behavior.properties.forEach(prop => {
+      let descriptor = {
+        get: function() {
+          return this.au.controller.viewModel[prop.name];
+        },
+        set: function(value) {
+          this.au.controller.viewModel[prop.name] = value;
+        }
+      };
+
+      descriptor.get.getObserver = function(obj) {
+        return getObserver(behavior, obj.au.controller.viewModel, prop.name);
+      };
+
+      Object.defineProperty(proto, prop.name, descriptor);
+    });
+
+    Object.keys(behavior.target.prototype).forEach(key => {
+      let value = behavior.target.prototype[key];
+
+      if(typeof value === 'function') {
+        proto[key] = function(...args) {
+          return this.au.controller.viewModel[key](...args);
+        };
+      }
+    });
 
     return proto;
   }
+}
+
+function getObserver(behavior, instance, name) {
+  let lookup = instance.__observers__;
+
+  if (lookup === undefined) {
+    if (!behavior.isInitialized) {
+      behavior.initialize(Container.instance || new Container(), instance.constructor);
+    }
+
+    lookup = behavior.observerLocator.getOrCreateObserversLookup(instance);
+    behavior._ensurePropertiesDefined(instance, lookup);
+  }
+
+  return lookup[name];
 }
