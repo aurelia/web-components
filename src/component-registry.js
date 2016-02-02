c9bb7fff0b1ffbd6013e17732839275cac66f59b
Rob Eisenberg <rob@bluespire.com>
2016-02-02 02:31:20 -0500

    feat(all): finish basic component creation logic

--- src/component-registry.js
+++ src/component-registry.js
@@ -1,5 +1,15 @@
 import {inject, Container} from 'aurelia-dependency-injection';
-import {ViewCompiler, ViewResources, BehaviorInstruction} from 'aurelia-templating';
+import {DOM} from 'aurelia-pal';
+import {
+  ViewCompiler,
+  ViewResources,
+  BehaviorInstruction,
+  TargetInstruction,
+  BoundViewFactory,
+  ViewSlot
+} from 'aurelia-templating';
+
+let emptyArray = Object.freeze([]);
 
 @inject(Container, ViewCompiler, ViewResources)
 export class ComponentRegistry {
@@ -11,7 +21,7 @@ export class ComponentRegistry {
     this.viewResources = viewResources;
   }
 
-  register(behavior, tagName?:string) {
+  registerBehavior(behavior, tagName?:string) {
     let proto = this._createWebComponentPrototypeFromBehavior(behavior);
     tagName = this._determineTagName(behavior, tagName);
     this._lookup[tagName] = {
@@ -35,33 +45,60 @@ export class ComponentRegistry {
     let proto = Object.create(HTMLElement.prototype, {
       createdCallback: {
         value: function() {
-          let instruction = BehaviorInstruction.element(this, behavior);
+          let behaviorInstruction = BehaviorInstruction.element(this, behavior);
           let attributes = this.attributes;
+          let children = this._children = [];
+          let bindings = this._bindings = [];
+
+          //TODO: processAttributes
 
           for (let i = 0, ii = attributes.length; i < ii; ++i) {
             attr = attributes[i];
-            instruction.attributes[attr.name] = attr.value;
+            behaviorInstruction.attributes[attr.name] = attr.value;
           }
 
-          behavior.compile(compiler, viewResources, this, instruction, this.parentNode);
+          behavior.compile(compiler, viewResources, this, behaviorInstruction, this.parentNode);
 
-          let childContainer = container.createChild();
-          //TODO: set up container with local entities
+          let targetInstruction = TargetInstruction.normal(
+            0,
+            0,
+            [behavior.target],
+            [behaviorInstruction],
+            emptyArray,
+            behaviorInstruction
+          );
 
-          let controller = behavior.create(childContainer, instruction, this)
+          let childContainer = createElementContainer(
+            container,
+            this,
+            targetInstruction,
+            behaviorInstruction.partReplacements,
+            children,
+            viewResources
+          );
+
+          let controller = behavior.create(childContainer, behaviorInstruction, this, bindings);
           controller.created(null);
         }
       },
       attachedCallback: {
         value: function() {
           this.au.controller.bind();
+          this._bindings.forEach(x => x.bind());
+          this._children.forEach(x => x.bind());
+
           this.au.controller.attached();
+          this._children.forEach(x => x.attached());
         }
       },
       detachedCallback: {
         value: function() {
           this.au.controller.detached();
+          this._children.forEach(x => x.detached());
+
           this.au.controller.unbind();
+          this._bindings.forEach(x => x.unbind());
+          this._children.forEach(x => x.unbind());
         }
       },
       attributeChangedCallback: {
@@ -118,4 +155,64 @@ function getObserver(behavior, instance, name) {
   }
 
   return lookup[name];
+}
+
+function elementContainerGet(key) {
+  if (key === DOM.Element) {
+    return this.element;
+  }
+
+  if (key === BoundViewFactory) {
+    if (this.boundViewFactory) {
+      return this.boundViewFactory;
+    }
+
+    let factory = this.instruction.viewFactory;
+    let partReplacements = this.partReplacements;
+
+    if (partReplacements) {
+      factory = partReplacements[factory.part] || factory;
+    }
+
+    this.boundViewFactory = new BoundViewFactory(this, factory, partReplacements);
+    return this.boundViewFactory;
+  }
+
+  if (key === ViewSlot) {
+    if (this.viewSlot === undefined) {
+      this.viewSlot = new ViewSlot(this.element, this.instruction.anchorIsContainer);
+      this.element.isContentProjectionSource = this.instruction.lifting;
+      this.children.push(this.viewSlot);
+    }
+
+    return this.viewSlot;
+  }
+
+  if (key === ViewResources) {
+    return this.viewResources;
+  }
+
+  if (key === TargetInstruction) {
+    return this.instruction;
+  }
+
+  return this.superGet(key);
+}
+
+function createElementContainer(parent, element, instruction, children, partReplacements, resources) {
+  let container = parent.createChild();
+  let provider = instruction.providers[0];
+
+  container.element = element;
+  container.instruction = instruction;
+  container.children = children;
+  container.viewResources = resources;
+  container.partReplacements = partReplacements;
+
+  container.registerSingleton(provider, provider);
+
+  container.superGet = container.get;
+  container.get = elementContainerGet;
+
+  return container;
 }
